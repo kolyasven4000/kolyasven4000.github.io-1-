96991e3603c64d96e09be981a6f2984590e5dc6b
kolysven4000 <kolyasven74@gmail.com>
2016-01-31 12:42:36 +0300

    remake main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-
+(function(){
 var popup = document.querySelector(".header__top-menu");
 var menuShow = popup.querySelector(".menu-toggle");
 var mobileMenu = popup.querySelector(".mobile-menu");
@@ -7,19 +7,13 @@ var mobileMenu = popup.querySelector(".mobile-menu");
 menuShow.addEventListener("click", function(event){
 		event.preventDefault();
 
-
 	popup.classList.toggle("header__top-menu--open");
 	menuShow.classList.toggle("menu-toggle--active");
-	mobileMenu.classList.toggle("mobile-menu--show");
-	
+	mobileMenu.classList.toggle("mobile-menu--show");	
 });
 
-
-
-
-
-
 var mql = window.matchMedia("(min-width: 960px)");
+//убираю ненужные классы на мидл ширине окна
 function delForWidth(mql){
   if (mql.matches) {
 	  mobileMenu.classList.remove("mobile-menu--show");
@@ -27,5 +21,8 @@ function delForWidth(mql){
 	  menuShow.classList.remove("menu-toggle--active");
 	}
 }
+//отслеживает срабатывание события
 mql.addListener(delForWidth);
-delForWidth(mql);
+
+})();
+
